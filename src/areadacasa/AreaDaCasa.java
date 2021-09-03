05badff589244cb995217519354cc0b9f96c4abb
marcus212 <marcusvinniciusouza@gmail.com>
2021-09-03 16:55:27 -0300

    areadacasa

---
+++ src/areadacasa/AreaDaCasa.java
@@ -0,0 +1,17 @@
+package areadacasa;
+
+import java.util.Scanner;
+
+public class AreaDaCasa {
+  
+    public static void main(String[] args) {
+		
+    	Scanner leitor = new Scanner (System.in);
+    	
+    	int altura,largura;
+    	int contador = 0;
+    	
+    	// não concluido//
+	}	
+	
+}
