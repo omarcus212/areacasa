0d1ac22929404f879cfbcd95cc302e277a2b9b40
omarcus212 <marcusvinniciusouza@gmail.com>
2021-09-09 16:12:11 -0300

    origin areadacasa

--- src/areadacasa/AreaDaCasa.java
+++ src/areadacasa/AreaDaCasa.java
@@ -2,16 +2,52 @@ package areadacasa;
 
 import java.util.Scanner;
 
+import javax.print.attribute.standard.Compression;
+
 public class AreaDaCasa {
   
     public static void main(String[] args) {
 		
     	Scanner leitor = new Scanner (System.in);
     	
-    	int altura,largura;
-    	int contador = 0;
     	
-    	// não concluido//
+    	double largura,comprimento,total;
+    	String resposta = "s";
+    	String comodo;
+        double areatotal = 0;
+    	
+    	
+    	while(resposta.equals("s")) {
+    		
+    		System.out.print("qual o comodo: ");
+    		comodo = leitor.next();
+    		
+    		System.out.print ("qual a largura: ");
+    		largura = leitor.nextInt();
+    		
+    		System.out.print("qual a comprimento: ");
+    		comprimento = leitor.nextInt();
+    		
+    		total=largura*comprimento;
+    		System.out.println("resultado é  = "+total);
+    		
+    		
+    		
+    		System.out.print("quer continuar(s),(n): ");
+    		resposta = leitor.next();
+    		
+    		total=total+areatotal;
+    		
+    		
+    	}
+    	System.out.println("finalizado: " +areatotal);
+    	leitor.close();
+    	 
+    	
+    	
+    	
+    	
+    	
 	}	
 	
 }
